4955961ccce6d38285d6bb69a251e5bf9ecdfc4b
joshuabowersChurch <joshua.bowers@churchofjesuschrist.org>
2022-08-30 16:07:57 -0600

    got company and product gen working

--- scripts/genCompany.js
+++ scripts/genCompany.js
@@ -1,7 +1,7 @@
 const fs = require( "fs" )
 const path = require( "path" );
 
-const { faker:{ internet, company, address, helpers, date } } = require( "@faker-js/faker" );
+const { faker:{ internet, company, address, helpers, date, phone } } = require( "@faker-js/faker" );
 const uuid = require( "uuid" ).v4;
 
 
@@ -11,6 +11,7 @@ function  buildURL( domain, subDomain, path ){
 
 
 const domain = internet.domainName();
+const state = address.state()
 
 const keywordScalar = 5;
 const createdOn = date.between( "2000-01-01T00:00:00", (new Date()).toISOString() )
@@ -32,7 +33,8 @@ for( let i = 0, l = Math.floor( Math.random() * emailScalar ); i < l; ++i ){
     type = types[i];
 
     emails.push({
-        type:    type,
+        id:      uuid(),
+        purpose: type,
         address: internet.email( type, "", domain, { allowSpecialCharacters: false } )
     })
 }
@@ -47,41 +49,84 @@ for( let i = 0, l = Math.floor( Math.random() * domainScalar ); i < l; ++i ){
     if( l > types.length ){
         subDomain = internet.domainWord()
         domains.push({
-            type:    "subDomain",
+            id:      uuid(),
+            purpose: "subDomain",
             address: buildURL( domain, subDomain )
         })
     } else {
         domains.push({
-            type:    type,
+            id:      uuid(),
+            purpose: type,
             address: buildURL( domain, type )
         })
     }
 }
 
+
+const phones = [];
+const phoneScalar = 10
+const phoneTypes = [ "sales", "support", "internal", "warehouses", "services", "operator" ];
+
+for( let i = 0, l = Math.floor( Math.random() * phoneScalar ); i < l; ++i ){
+    
+    phones.push({
+        id:      uuid(),
+        purpose: helpers.shuffle( phoneTypes )[0],
+        number:  phone.number( "###-###-####" )
+    })
+}
+
+const subsidiaries = [];
+const subsidiaryScalar = 2
+
+for( let i = 0, l = Math.floor( Math.random() * subsidiaryScalar ); i < l; ++i ){
+    
+    subsidiaries.push({
+        id:           uuid(),
+        parentId:     uuid(),
+        number:       company.name()
+    })
+}
+
+const parentTo = [];
+const parentScalar = 4
+
+for( let i = 0, l = Math.floor( Math.random() * parentScalar ); i < l; ++i ){
+    parentTo.push({
+        id:           uuid(),
+        subsidiaryId: uuid(),
+        number:       company.name()
+    })
+}
+
 fs.writeFileSync(
     path.join( __dirname, "company.json"),
     JSON.stringify({
-        id:          uuid(),
-        name:        company.name(),
-        suffix:      company.companySuffix(),
-        status:      helpers.shuffle(['CREATED', 'REVIEWED', 'PUBLISHED', 'REVOKED'])[0],
-        website:     domain,
-        slogan:      company.catchPhrase(),
-        keywords:    keywords,
+        id:       uuid(),
+        name:     company.name(),
+        suffix:   company.companySuffix(),
+        status:   helpers.shuffle(['CREATED', 'REVIEWED', 'PUBLISHED', 'REVOKED'])[0],
+        website:  domain,
+        slogan:   company.catchPhrase(),
+        keywords: keywords,
 
-        soldBy:      uuid(),
+        soldBy:   uuid(),
 
-        email: emails,
+        email:  emails,
         domain: domains,
+        phone:  phones,
+
+        subsidiaryOf: subsidiaries,
+        parentOf:     parentTo,
 
         companyHQ: {
-            street:      "400 S 205 E",
-            city:        "Salt Lake City",
-            state:       "Utah",
-            country:     "United States of America",
-            countryCode: "US",
-            latitude:    40.759926,
-            longitude:   -111.884888
+            street:      address.streetAddress(),
+            city:        address.cityName(),
+            state:       state,
+            country:     address.country(),
+            postal:      address.zipCodeByState( state ),
+            latitude:    address.latitude(),
+            longitude:   address.longitude()
         },
 
         metadata:{
